b9f90fb05e0ee0f888a355b6b5dc56aec13b67d4
Kopkap <imkopkap@gmail.com>
2019-01-18 14:28:52 +0700

    Create Lab1.sql

---
+++ Lab1.sql
@@ -0,0 +1,47 @@
+USE Lab1;
+
+--DROP DATABASE testDB;
+--GO
+
+--CREATE DATABASE testDB;
+--GO
+
+--CREATE TABLE Students (
+--	StudentID int,
+--	FirstName varchar(255),
+--	LastName varchar(255),
+--	NickName varchar(255),
+--	Email varchar(255),
+--	PhoneNumber varchar(10),
+--	Sex varchar(255),
+--	BirthDate date
+--);
+--GO
+
+--INSERT INTO Students 
+--VALUES (5988014, 'Sarayut', 'Lawilai', 'Kopkap', 'imkopkap.se@gmail.com', '0922524950', 'Male', '10 October 1997');
+--GO
+
+--INSERT INTO Students VALUES 
+--(5988199, 'Tanakitti', 'Sachati', 'Bank', 'bank21235.dev@gmail.com', '0922524951', 'Male', '13 December 1997'),
+--(5988200, 'Thanadon', 'Bookerd', 'Tan', 'tan@gmail.com', '0922223294', 'Male', '17 May 1997'),
+--(5988045, 'Chanatip', 'Ponprasit', 'Oat', 'chanatip.oat@gmail.com', '0846657123', 'Male', '15 January 1996');
+--GO
+
+--UPDATE Students 
+--SET PhoneNumber = '0924789336'
+--WHERE FirstName = 'Tanakitti'
+
+
+--UPDATE Students 
+--SET PhoneNumber = '0984761961'
+--WHERE FirstName = 'Thanadon'
+
+--UPDATE Students 
+--SET PhoneNumber = '0837923421'
+--WHERE FirstName = 'Chanatip'
+
+--DELETE FROM Students
+--WHERE FirstName = 'Chanatip';
+
+--SELECT * FROM Students;
